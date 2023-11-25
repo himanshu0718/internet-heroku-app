af49e00ccd0c9686fe17a6999f1da2a7a82b79a4
Himanshu Sharma <himanshucr718lee@gmail.com>
2023-11-25 18:59:06 +0530

    authentication and dissapair

---
+++ 06_dissapair_elements.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+from selenium.webdriver.common.by import By
+
+driver.get("https://the-internet.herokuapp.com/")
+driver.maximize_window()
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//a[normalize-space()='Disappearing Elements']").click()
+gallery_count = 0  
+no_gallery = 0
+n = 5
+for i in range(n):
+    try:
+        if driver.find_element(By.XPATH,"//a[normalize-space()='Gallery']"):
+            gallery_count+=1
+    except:
+        no_gallery +=1
+    driver.refresh()
+    time.sleep(1)
+
+#we are able to print the number of times gallery element is present after refreshing the webpage n number of times.
+
+print(f"Out of {n} times Gallery was present {gallery_count} times")
+print(f"Out of {n} times Dissapaired {no_gallery} times")
