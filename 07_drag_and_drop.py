3ea88e5fa7800760b22043c5e6cf4afb6a0ecbb1
Himanshu Sharma <himanshucr718lee@gmail.com>
2023-11-25 23:14:29 +0530

    7,8

--- 07_drag_and_drop.py
+++ 07_drag_and_drop.py
@@ -4,18 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
 
 driver.get("https://the-internet.herokuapp.com/")
 driver.maximize_window()
 time.sleep(1)
 
-driver.find_element(By.XPATH,"//a[normalize-space()='Digest Authentication']").click()
+driver.find_element(By.XPATH,"//a[normalize-space()='Drag and Drop']").click()
 time.sleep(1)
-driver.get("http://admin:admin@the-internet.herokuapp.com/digest_auth")
-time.sleep(3)
-if driver.find_element(By.XPATH,"//p[contains(text(),'Congratulations! You must have the proper credenti')]"):
-    print("AUTHENTICATION SUCCESSFUL")
-else:
-    print("Failed")
 
+source_element = driver.find_element(By.XPATH,"//div[@id='column-a']")
+destination = driver.find_element(By.XPATH,"//div[@id='column-b']")
+
+action = ActionChains(driver)
+action.click_and_hold(source_element).move_to_element(destination).release(destination).perform()
+time.sleep(3)
+print("Pass")
 
---
+++ 08_dropdown.py
@@ -0,0 +1,38 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+driver.get("https://the-internet.herokuapp.com/")
+driver.maximize_window()
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//a[normalize-space()='Dropdown']").click()
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//select[@id='dropdown']").click()
+time.sleep(1)
+
+Select(driver.find_element(By.XPATH,"//select[@id='dropdown']")).select_by_value("1")
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//select[@id='dropdown']").click()
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//select[@id='dropdown']").click()
+time.sleep(1)
+
+Select(driver.find_element(By.XPATH,"//select[@id='dropdown']")).select_by_value("2")
+time.sleep(1)
+
+driver.find_element(By.XPATH,"//select[@id='dropdown']").click()
+time.sleep(1)
+
+print("Pass")
+
+
+
